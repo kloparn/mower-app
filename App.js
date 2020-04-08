3bbd1a4ef18d57de2aabff48d7e0d3a5cadd59d8
BuildTools <unconfigured@null.spigotmc.org>
2020-04-08 12:41:21 +0200

    Added support for styled-components library

--- App.js
+++ App.js
@@ -6,7 +6,9 @@
  * @flow
  */
 
-import React, { Fragment, useState, useEffect } from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
+import styled from 'styled-components';
+
 import {
   SafeAreaView,
   StyleSheet,
@@ -16,7 +18,7 @@ import {
   StatusBar,
   PermissionsAndroid,
   Alert,
-  Button
+  Button,
 } from 'react-native';
 
 import {
@@ -26,27 +28,25 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import { BleManager } from 'react-native-ble-plx';
+import {BleManager} from 'react-native-ble-plx';
 
-
+// Make sure to clean up the code, NO STUPID TEXT.
 const requestLocationPermission = async () => {
   try {
     const granted = await PermissionsAndroid.request(
       PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
       {
-        title: "Cool Photo App Location Permission",
-        message:
-          "We need your location" +
-          "So we can find your dog",
-        buttonNeutral: "Ask Me Later",
-        buttonNegative: "Cancel",
-        buttonPositive: "OK"
-      }
+        title: 'Cool Photo App Location Permission',
+        message: 'We need your location' + 'So we can find your dog', // LIKE THIS BITCH
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
     );
     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log("Now we have you data/dog");
+      console.log('Now we have you data/dog');
     } else {
-      console.log("Apple failed to steal your data");
+      console.log('Apple failed to steal your data');
     }
   } catch (err) {
     console.warn(err);
@@ -61,96 +61,60 @@ const App = () => {
     manager.startDeviceScan(null, null, (error, device) => {
       if (error) {
         console.log(error);
-      }
-      else {
+      } else {
         //console.warn("dev", devices);
-        const devAlreadyExist = devices.find(d => d.id === device.id);
-        if (!devAlreadyExist)
-          setDevices([...devices, device]);
+        const devAlreadyExist = devices.find((d) => d.id === device.id);
+        if (!devAlreadyExist) setDevices([...devices, device]);
         console.log(device.id);
       }
-    })
-
-  }
+    });
+  };
   const resetDeviceScan = () => {
     setDevices([]);
     deviceScan();
-  }
+  };
   deviceScan();
 
   useEffect(() => {
     // Create an scoped async function in the hook
-    async function functionName() {
-
-
-    }
+    async function functionName() {}
     // Execute the created function directly
     functionName();
   }, []);
-
 
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <View style={styles.container}>
-          <Button title="Request Location permission" onPress={requestLocationPermission} />
-        </View>
         <View>
-          <Text>Searching...</Text>
-          {devices.map(d => {
+          <Button
+            title="Request Location permission"
+            onPress={requestLocationPermission}
+          />
+        </View>
+        <MainView>
+          <MainText>Searching...</MainText>
+          {devices.map((d) => {
             return (
               <Fragment key={d.id}>
-                <Text>Name:{(d.name === null) ? "null" : d.name}</Text>
-                <Text>Id:{d.id}</Text>
+                <MainText>Name:{d.name === null ? 'null' : d.name}</MainText>
+                <MainText>Id:{d.id}</MainText>
               </Fragment>
-            )
+            );
           })}
-        </View>
+        </MainView>
         <Button title="Scan for devices" onPress={resetDeviceScan} />
-
       </SafeAreaView>
     </Fragment>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+// TEMP
+const MainView = styled.View`
+  background-color: black;
+`;
+const MainText = styled.Text`
+  color: white;
+`;
 
 export default App;
