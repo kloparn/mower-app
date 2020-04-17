2e35a4226d98aa5d916502ce29fe5e81508a7a13
Anton <anton_real_madrid@icloud.com>
2020-04-17 22:15:24 +0200

    Added styled components theme

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
 import {StoreProvider, createStore} from 'easy-peasy';
+import {ThemeProvider} from 'styled-components';
 
 import model from './store/model';
+import {defaultTheme} from './styles/themes';
 import {MainScreen, ControlScreen, VisualizationScreen} from './screens';
 
 const store = createStore(model);
@@ -19,27 +20,29 @@ const Tab = createBottomTabNavigator();
 const App = () => {
   return (
     <StoreProvider store={store}>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({route}) => ({
-            tabBarIcon: ({focused, color, size}) => {
-              switch (route.name) {
-                case 'Home':
-                  return <Icon name="home" size={size} color={color} />;
-                case 'Control':
-                  return <Icon name="bluetooth" size={size} color={color} />;
-                case 'Visualization':
-                  return <Icon name="map" size={size} color={color} />;
-                default:
-                  return <Icon name="question" size={size} color={color} />;
-              }
-            },
-          })}>
-          <Tab.Screen name="Control" component={ControlScreen} />
-          <Tab.Screen name="Home" component={MainScreen} />
-          <Tab.Screen name="Visualization" component={VisualizationScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <ThemeProvider theme={defaultTheme}>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({route}) => ({
+              tabBarIcon: ({focused, color, size}) => {
+                switch (route.name) {
+                  case 'Home':
+                    return <Icon name="home" size={size} color={color} />;
+                  case 'Control':
+                    return <Icon name="bluetooth" size={size} color={color} />;
+                  case 'Visualization':
+                    return <Icon name="map" size={size} color={color} />;
+                  default:
+                    return <Icon name="question" size={size} color={color} />;
+                }
+              },
+            })}>
+            <Tab.Screen name="Control" component={ControlScreen} />
+            <Tab.Screen name="Home" component={MainScreen} />
+            <Tab.Screen name="Visualization" component={VisualizationScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
     </StoreProvider>
   );
 };
---
+++ src/styles/themes.js
@@ -0,0 +1,9 @@
+export const defaultTheme = {
+  colors: {
+    primary: '#071a52',
+    secondary: '#086972',
+    third: '#17b978',
+    forth: '#a7ff83',
+    text: '#fff',
+  },
+};
