c75da79cb1ba17c791f82d382456137a46d2a373
hoa <johan.kahlman91@gmail.com>
2020-04-24 11:14:46 +0200

    #B2.5 Canvas import

--- src/screens/VisualizationScreen.js
+++ src/screens/VisualizationScreen.js
@@ -1,4 +1,5 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
+import Canvas from 'react-native-canvas'; 
 import {useStoreState, useStoreActions} from 'easy-peasy';
 import {
   SafeAreaView,
@@ -14,11 +15,19 @@ import {
 
 import {Layout} from '../components';
 
+
+handleCanvas = (canvas) => {
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'purple';
+  ctx.fillRect(0, 0, 100, 100); 
+}
+
 const VisualizationScreen = () => {
   return (
     <Layout>
       <View>
         <Text>Hej backend :)))))))))))))</Text>
+        <Canvas ref={this.handleCanvas}/>
       </View>
     </Layout>
   );
