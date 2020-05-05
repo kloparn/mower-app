37d5365e3518d7d17d5bf0450004aa4e457235ee
kloparn1 <adam97.live.com@live.com>
2020-05-05 14:03:30 +0200

    yeet this code

--- src/components/LineSensorIndicator.js
+++ src/components/LineSensorIndicator.js
@@ -7,6 +7,8 @@ import {Button, View, Text, addons} from 'react-native';
 
 const LineSensorIndicator = () => {
   const {lineSensor} = useStoreState((state) => state.bluetooth);
+
+  // Made the picture a const to be used in both statements, so it does not need to be init in both statements
   const picture = <Icon name="grip-lines" size={60} color="#FFFFFF" />;
 
   return lineSensor ? (
