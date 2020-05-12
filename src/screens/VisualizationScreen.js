e0001656a628821dcb0317788c158e5a084e6f2b
Yihaa9 <isja1709@student.ju.se>
2020-05-12 14:13:01 +0200

    Visualization update

--- src/screens/VisualizationScreen.js
+++ src/screens/VisualizationScreen.js
@@ -1,5 +1,5 @@
 import React, {Fragment, Component} from 'react';
-import Canvas from 'react-native-canvas';
+import Canvas from 'react-native-canvas'; 
 import {useStoreState, useStoreActions} from 'easy-peasy';
 import {
   SafeAreaView,
@@ -11,104 +11,122 @@ import {
   PermissionsAndroid,
   Alert,
   Button,
-  Dimensions,
+  Dimensions
 } from 'react-native';
 import {Layout} from '../components';
 
-class VisualizationScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      session: [{position: {x: 0, y: 0}}],
-    };
-  }
+  // #B2.5 The path and collision avoidance events shall be visualized on the App with data sent by the webserver.
+  class VisualizationScreen extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        session: [
+          {position: {x: 0, y: 0}}]
+       };
+     }
 
-  handleCanvas = (canvas) => {
-    canvas.width = Dimensions.get('window').width; //max X
-    canvas.height = Dimensions.get('window').width; // max Y
+     handleCanvas = (canvas) => {
 
-    var robotSize = 15;
-    var session = this.state.session;
+      var screenDimension = Dimensions.get('window').width;
+      canvas.width =  screenDimension
+      canvas.height = screenDimension 
 
-    var maxX = 1,
-      maxY = 1;
-    for (var i = 0; i < session.length; i++) {
-      if (Math.abs(session[i].position.x) > maxX)
-        maxX = Math.abs(session[i].position.x);
-      if (Math.abs(session[i].position.y) > maxY)
-        maxY = Math.abs(session[i].position.y);
+      const LINE_SENSOR = 1
+      const ULTRA_SONIC_SENSOR = 2
+      
+      const ctx = canvas.getContext('2d');
+      const session = this.state.session
+      const origo = screenDimension/2
+
+      var robotSize = 1
+      var maxCoordinate = 75
+      var pixelRatio
+      var x, y
+
+      //Find Max coordinate
+      for(var i = 0; i < session.length; i++) {
+        if(Math.abs(session[i].position.x) > maxCoordinate)
+          maxCoordinate = Math.abs(session[i].position.x)
+        if(Math.abs(session[i].position.y) > maxCoordinate)
+          maxCoordinate = Math.abs(session[i].position.y)
+      }
+
+      //Used for scaling
+      pixelRatio = (screenDimension - 50) / maxCoordinate / 2
+      
+      robotSize *= pixelRatio    
+ 
+      //Draw the lines of movement
+      ctx.beginPath();
+      for(var i = 0; i < session.length; i++) {
+        x = (origo + session[i].position.x * pixelRatio)
+        y = (origo + session[i].position.y * pixelRatio* - 1)
+        ctx.lineTo(x, y);
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = robotSize;
+        ctx.lineJoin = 'round';
+        ctx.stroke();
+      }
+
+      //Draw avoidances
+      for(var i = 0; i < session.length; i++) {
+        x = (origo + session[i].position.x * pixelRatio)
+        y = (origo + session[i].position.y * pixelRatio* - 1)
+
+        //Line avoidances
+        if(session[i].flag == LINE_SENSOR) {
+          ctx.beginPath();
+          ctx.arc(x, y, robotSize/4, 0, Math.PI * 2, false);
+          ctx.strokeStyle = 'grey';
+          ctx.stroke();
+        } 
+        //Object avoidances
+        else if (session[i].flag == ULTRA_SONIC_SENSOR) {
+          ctx.beginPath();
+          ctx.arc(x, y, robotSize/4, 0, Math.PI * 2, false);
+          ctx.strokeStyle = 'red';
+          ctx.stroke();
+        }     
+      }
+
+      //Draw Robot
+      ctx.beginPath();
+      ctx.arc(x, y, robotSize/2, 0, Math.PI * 2, false);
+      ctx.strokeStyle = 'green';
+      ctx.stroke();
+
+      this.setState({canvas: canvas})
+    }
+   
+    
+    getSession = () => { //#B2.3 A REST API shall handle data communication between the App and webserver.
+      fetch("https://leather-batend.herokuapp.com/api/session", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "api_key": "sdfasdfasdfasdfa"
+        }
+         })
+    .then(res => res.json())
+        .then(session => {
+          this.setState({session: session}, () => {
+            this.handleCanvas(this.state.canvas)
+          })
+        })
+        .catch(err => {console.log(err)}); 
     }
 
-    var longSide, shortSide;
-    if (maxX > maxY) {
-      longSide = maxX;
-      shortSide = maxY;
-    } else {
-      longSide = maxY;
-      shortSide = maxX;
-    }
-
-    var sideRatio = shortSide / longSide;
-    const pixelMax = canvas.width / 2 - 10;
-
-    const xRatio =
-      maxX == shortSide ? (pixelMax / maxX) * sideRatio : pixelMax / maxX;
-    const yRatio =
-      maxY == shortSide ? (pixelMax / maxY) * sideRatio : pixelMax / maxY;
-    const xOrigo = canvas.width / 2;
-    const yOrigo = canvas.height / 2;
-
-    const ctx = canvas.getContext('2d');
-    ctx.beginPath();
-    var x, y;
-    for (var i = 0; i < session.length; i++) {
-      x = xOrigo + session[i].position.x * xRatio;
-      y = yOrigo + session[i].position.y * yRatio * -1;
-      ctx.lineTo(x, y);
-    }
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = robotSize;
-    ctx.lineJoin = 'round';
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.arc(x, y, 7, 0, Math.PI * 2, false);
-    ctx.strokeStyle = 'green';
-    ctx.stroke();
-
-    this.setState({canvas: canvas});
-  };
-
-  getSession = () => {
-    fetch('https://leather-batend.herokuapp.com/api/session', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        api_key: 'sdfasdfasdfasdfa',
-      },
-    })
-      .then((res) => res.json())
-      .then((session) => {
-        this.setState({session: session}, () => {
-          this.handleCanvas(this.state.canvas);
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  render() {
-    return (
-      <Layout>
-        <View>
-          {this.populateTest}
-          <Canvas ref={this.handleCanvas} />
-          <Button onPress={this.getSession} title="Get path"></Button>
-        </View>
-      </Layout>
-    );
-  }
-}
+     render () {
+      return (
+        <Layout>
+          <View>
+            {this.populateTest}
+            <Canvas ref={this.handleCanvas} />
+            <Button onPress={this.getSession} title="Get path"></Button>
+          </View>
+        </Layout>
+      );
+     }
+};
 
 export default VisualizationScreen;
